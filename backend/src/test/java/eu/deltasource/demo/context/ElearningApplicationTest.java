01f60695f107e1e63a06f40e0120a8dadb4aeaba
mihaelakolarova03 <mihaelakolarova@deltasource.eu>
2025-01-29 13:24:33 +0200

    Add Spring Boot tests for ElearningApplication class

---
+++ backend/src/test/java/eu/deltasource/demo/context/ElearningApplicationTest.java
@@ -0,0 +1,13 @@
+package eu.deltasource.demo.context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ElearningApplicationTest {
+
+    @Test
+    void contextLoads(){
+
+    }
+}
